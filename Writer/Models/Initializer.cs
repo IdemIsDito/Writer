1e08bfc3e38f2c597d04196cdd6bf2de921640a1
IdemIsdito <jeroenrwever@gmail.com>
2015-02-15 15:24:47 +0100

    Do not create records on Init

--- Writer/Models/Initializer.cs
+++ Writer/Models/Initializer.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 
 namespace Writer.Models
 {
@@ -8,11 +6,6 @@ namespace Writer.Models
 	{
 		protected override void Seed(WriterContext context)
 		{
-			var stories = new List<ClassicActivity>
-			{
-				new ClassicActivity { Id = 1, Summary = "Vivamus feugiat commodo volutpat. Sed sed velit libero. Sed enim magna, commodo ac volutpat quis, sodales non orci. Quisque eget urna magna. Maecenas sollicitudin mauris odio, id pretium dui aliquet eget. Donec hendrerit dui in tellus ornare, quis rhoncus diam condimentum. Donec commodo lorem in elit aliquam dapibus."}
-			};
-			stories.ForEach(s => context.ClassicActivities.Add(s));
 			context.SaveChanges();
 		}
 	}
